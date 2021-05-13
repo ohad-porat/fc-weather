c345ecf665f69634188e8b3d962ba5e886eeb8b5
Ohad Porat <ohadp23@gmail.com>
2021-05-13 18:37:17 -0400

    created a function to get the current location

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import React, { useEffect, useState } from "react"
+import { StyleSheet, Text, SafeAreaView } from "react-native"
+import * as Location from "expo-location"
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null)
+  console.log("yoo")
+  const load = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== "granted") {
+        setErrorMessage("Access to location is needed to run the app")
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+      const { latitude, longitude } = location.coords
+      alert(`latitude: ${latitude} longitude: ${longitude}`)
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    load()
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>FC Weather</Text>
       <StatusBar style="auto" />
-    </View>
-  );
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#32a8f0",
+    alignItems: "center",
   },
-});
+})
