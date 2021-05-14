08fd728bd165519bc77dad503086a35a7833b5f0
Ohad Porat <ohadp23@gmail.com>
2021-05-14 17:10:12 -0400

    added a fetch request to openweather api

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import { StatusBar } from "expo-status-bar"
 import React, { useEffect, useState } from "react"
 import { StyleSheet, Text, SafeAreaView } from "react-native"
 import * as Location from "expo-location"
+import { WEATHER_API_KEY } from "react-native-dotenv"
+
+const baseWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?`
 
 export default function App() {
-  const [errorMessage, setErrorMessage] = useState(null)
-  
-  const load = async () => {
+  const [errorMessage, setErrorMessage] = useState()
+  const [currentWeather, setCurrentWeather] = useState()
+
+  const getCurrentWeather = async () => {
     try {
       let { status } = await Location.requestForegroundPermissionsAsync()
       if (status !== "granted") {
@@ -15,28 +19,49 @@ export default function App() {
       }
       const currentLocation = await Location.getCurrentPositionAsync()
       const { latitude, longitude } = currentLocation.coords
-      alert(`latitude: ${latitude} longitude: ${longitude}`)
+
+      const currentLocationWeatherUrl = `${baseWeatherUrl}lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`
+
+      const response = await fetch(currentLocationWeatherUrl)
+      if (response.ok) {
+        const weatherData = await response.json()
+        setCurrentWeather(weatherData)
+      }
     } catch (error) {
       console.error(`Error in fetch: ${error.message}`)
     }
   }
 
   useEffect(() => {
-    load()
+    getCurrentWeather()
   }, [])
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text>FC Weather</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  )
+  if (currentWeather) {
+    const tempCelsius = Math.floor(currentWeather.main.temp)
+    const tempFahrenheit = Math.floor((tempCelsius * 1.8) + 32)
+
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>{tempCelsius}°C {"         "}{tempFahrenheit}°F</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    )
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text>Oh no! Something went wrong.</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#32a8f0",
+    backgroundColor: "#ffffff",
     alignItems: "center",
+    justifyContent: "center"
   },
 })
